e80dd5cf1692c4a7c698f2d8b5208efba093f6ef
MHDFahz <fahis.skazi@gmail.com>
2020-06-08 18:29:06 +0530

    Bugs

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,7 +1,10 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
@@ -12,6 +15,13 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'warning'
+app.config['MAIL_SERVER'] ='smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'fahis.skazi@gmail.com'
+app.config['MAIL_PASSWORD'] = "Shsh1998"
+mail = Mail(app)
+
 
 
 from flaskblog import routes
